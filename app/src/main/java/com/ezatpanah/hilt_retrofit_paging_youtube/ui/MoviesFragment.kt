75a76564b3609c9bd6de5a7bb8fa36724601df42
Abdulkadir LEVENT <abdulkadirleventqyahoo.com>
2023-12-02 01:08:35 +0300

    Update MoviesFragment.kt

--- app/src/main/java/com/ezatpanah/hilt_retrofit_paging_youtube/ui/MoviesFragment.kt
+++ app/src/main/java/com/ezatpanah/hilt_retrofit_paging_youtube/ui/MoviesFragment.kt
@@ -1,6 +1,5 @@
 package com.ezatpanah.hilt_retrofit_paging_youtube.ui
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -13,9 +12,7 @@ import androidx.core.view.isVisible
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
-import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.fragment.findNavController
 import androidx.paging.LoadState
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -39,7 +36,7 @@ class MoviesFragment : Fragment() {
 
     private val viewModel: MoviesViewModel by viewModels()
 
-     fun onCreateView2323(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    fun onCreateView2323(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         binding = FragmentMoviesBinding.inflate(layoutInflater, container, false)
         return binding.root
     }
@@ -54,7 +51,7 @@ class MoviesFragment : Fragment() {
         // Bu Fragment'ın kendi ActionBar'ını ayarla
         (activity as AppCompatActivity).setSupportActionBar(toolbar)
         (activity as AppCompatActivity).supportActionBar?.setDisplayHomeAsUpEnabled(true)
-       // (activity as AppCompatActivity).supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_round_star_24)
+        // (activity as AppCompatActivity).supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_round_star_24)
 
         toolbar.setNavigationOnClickListener {
             val drawerLayout: DrawerLayout = view.findViewById(R.id.drawerLayout)
@@ -112,45 +109,29 @@ class MoviesFragment : Fragment() {
         return view
     }
 
-    @SuppressLint("UnsafeRepeatOnLifecycleDetector")
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.apply {
             lifecycleScope.launch {
-                lifecycle.repeatOnLifecycle(Lifecycle.State.RESUMED) {
-                    viewModel.moviesList.collect {
-                       moviesAdapter.submitData(it)
-                    }
-                }
-            }
-            /*
-            lifecycleScope.launchWhenCreated {
                 viewModel.moviesList.collect {
                     moviesAdapter.submitData(it)
                 }
+
             }
-            */
             moviesAdapter.setOnItemClickListener {
                 val direction = MoviesFragmentDirections.actionMoviesFragmentToMovieDetailsFragment(it.id)
                 findNavController().navigate(direction)
             }
 
             lifecycleScope.launch {
-                lifecycle.repeatOnLifecycle(Lifecycle.State.RESUMED) {
-                    moviesAdapter.loadStateFlow.collect {
-                        val state = it.refresh
-                        prgBarMovies.isVisible = state is LoadState.Loading
-                    }
-                }
-            }
-            /*
-            lifecycleScope.launchWhenCreated {
                 moviesAdapter.loadStateFlow.collect {
                     val state = it.refresh
                     prgBarMovies.isVisible = state is LoadState.Loading
                 }
+
             }
-            */
+
             rlMovies.apply {
                 layoutManager = LinearLayoutManager(requireContext())
                 adapter = moviesAdapter
